84321ea49d64d9fb4cd07193429e1f350cb624d8
mokha <mokha@cisco.com>
2018-08-15 14:27:37 -0600

    configure webpack dev server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  devServer: {
+    host: process.env.HOST,
+    open: true,
+    overlay: true,
+    port: process.env.PORT,
+    stats: "errors-only",
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack demo',
