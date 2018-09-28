590e2438a85a5d0f61fb9f1d8891bd5db5d51994
mokha <mokha@cisco.com>
2018-09-28 14:36:22 -0600

    load images.

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,12 @@ const productionConfig = merge([
   parts.purifyCSS({
     paths: glob.sync(`${PATHS.app}/**/*.js`, { nodir: true }),
   }),
+  parts.loadImages({
+    options: {
+      limit: 15000,
+      name: "[name].[ext]",
+    },
+  }),
 ]);
 const developmentConfig = merge([
   parts.devServer({
@@ -31,6 +37,7 @@ const developmentConfig = merge([
     port: process.env.PORT,
   }),
   parts.loadCSS(),
+  parts.loadImages(),
 ]);
 
 module.exports = mode => {
--- webpack.parts.js
+++ webpack.parts.js
@@ -58,3 +58,19 @@ exports.autoprefix = () => ({
     plugins: () => [require("autoprefixer")()],
   }
 });
+
+exports.loadImages = ({ include, exclude, options } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        include,
+        exclude,
+        use: {
+          loader: "url-loader",
+          options,
+        },
+      }
+    ]
+  },
+});
