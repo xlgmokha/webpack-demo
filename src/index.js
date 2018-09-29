9d89327b22db2292fb7aa45c2f68bb63a5b56b44
mokha <mokha@cisco.com>
2018-09-29 10:30:19 -0600

    tree shaking.
    https://survivejs.com/webpack/optimizing/tree-shaking/

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import 'react-dom';
 import component from './component';
 import "./main.css";
 import 'purecss';
+import { bake } from "./shake";
 
+bake();
 document.body.appendChild(component());
