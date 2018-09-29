e4824d7ab69891db9c65e79ee6e2c3359d74bf7c
mokha <mokha@cisco.com>
2018-09-29 14:21:16 -0600

    Use a web worker to dispatch messages back and forth.
    * https://survivejs.com/webpack/techniques/web-workers/

--- src/component.js
+++ src/component.js
@@ -1,17 +1,17 @@
-export default (text = HELLO) => {
-  const element = document.createElement("div");
-  element.className = "pure-button";
-  element.innerHTML = text;
+import Worker from "worker-loader!./worker";
 
-  element.onclick = () => {
-    import("./lazy")
-      .then(lazy => {
-        element.textContent = lazy.default;
-      })
-      .catch(error => {
-        console.error(error);
-      })
-  }
+export default () => {
+  const element = document.createElement("h1");
+  const worker = new Worker();
+  const state = { text: "foo" };
+
+  worker.addEventListener("message", ({ data: { text } }) => {
+    state.text = text;
+    element.innerHTML = text;
+  });
+
+  element.innerHTML = state.text;
+  element.onclick = () => worker.postMessage({ text: state.text });
 
   return element;
 };
